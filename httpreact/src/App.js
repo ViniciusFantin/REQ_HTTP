758c2b3135f7fbef7a3f2270eb80f867584402f2
Vinicius Fantin <vinifantin302@gmail.com>
2025-02-14 17:53:18 -0300

    feat - perf

--- httpreact/src/App.js
+++ httpreact/src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+// Custom Hook
+import { useFetch } from "./hooks/useFetch";
+
+const url = "http://localhost:3000/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const { data: items } = useFetch(url);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  // resgatando dados
+  //useEffect(() => {
+  //async function fetchData() {
+  //const res = await fetch(url);
+
+  //const data = await res.json();
+
+  //setProducts(data);
+  //}
+  //fetchData();
+  //}, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    const addedProduct = await res.json();
+
+    setProducts((prevProduct) => [...prevProduct, addedProduct]);
+
+    setName("");
+    setPrice("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R${product.price}
+            </li>
+          ))}
+      </ul>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
