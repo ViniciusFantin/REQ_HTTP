382f97f19049d4c02f87c3b4712e97cc79410a1b
Vinicius Fantin <vinifantin302@gmail.com>
2025-02-14 18:13:04 -0300

    feat - callFetch

--- httpreact/src/hooks/useFetch.js
+++ httpreact/src/hooks/useFetch.js
@@ -3,6 +3,22 @@ import { useState, useEffect } from "react";
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
 
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    }
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(url);
@@ -11,7 +27,20 @@ export const useFetch = (url) => {
       setData(json);
     };
     fetchData();
-  }, [url]);
+  }, [url, callFetch]);
+
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+        const res = await fetch(...fetchOptions);
+        const json = await res.json();
+
+        setCallFetch(json);
+      }
+    };
+    httpRequest();
+  }, [config]);
 
   return { data };
 };
